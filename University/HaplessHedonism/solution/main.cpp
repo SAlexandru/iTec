f9a736e4f5ca7bd5a1498a4da266b3f5915cd058
SAlexandru <salexandru.alex@gmail.com>
2017-04-05 08:55:03 +0300

    added a,b,c solution

--- University/HaplessHedonism/solution/main.cpp
+++ University/HaplessHedonism/solution/main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 
-using namespace std;
+
 
 int main() {
-    unsigned long long int dp[1000011];
-
-    dp[3] = 0;
+    unsigned long long int dp[1000001];
+    
+    dp[0] = dp[1] = dp[2] = dp[3] = 0;
     dp[4] = 1;
-    for (int i = 5; i <= 1000000; ++i) {
-        int m = i >> 1;
+    for (unsigned long long int i = 5; i <= 1000000; ++i) {
+        unsigned long long int m = i >> 1;
         dp[i] = dp[i - 1] + m * (m - 1);
 
-        if (!(i & 1)) {
-           dp[i] -= m - 1; 
+        if ((i & 1) == 0) {
+          dp[i] -= m - 1;
         }
     }
-    
-    int x;
-    cin >> x;
-    while (cin >> x) {
-        cout << dp[x] << '\n';
+
+    int t, x;
+
+    for (std::cin >> t; t; --t) {
+        std::cin >> x;
+        std::cout << dp[x] << '\n';
     }
 
+
     return 0;
+
+
+
 }
