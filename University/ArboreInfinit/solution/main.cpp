79f9ab0c308922e759f24a51860c4c5bfcbd214c
mihaiSimu <simu.mihai@yahoo.com>
2017-04-06 22:11:09 +0300

    Finalized University Problems

--- University/ArboreInfinit/solution/main.cpp
+++ University/ArboreInfinit/solution/main.cpp
@@ -1,53 +1,65 @@
-#include <iostream>
- 
-using namespace std;
- 
-int msb(int x) {return 8*sizeof(int) - __builtin_clz(x) - 1; }
-int max(int x, int y) {return x > y ? x : y;}
- 
-bool equal(int x, int pos) {return ((1 << pos) & x) != 0;}
+#include <stdio.h>
 
-int lca2(int x, int y) {
-    if (x == y) return x;
-   
-    int idx = msb(x ^ y);
-
-    int diff = 1 << idx;
-    int ans = (x & ~(diff - 1)) | diff;
-
-    return ans & ~1;
+int getLevel(long a) {
+    if(a == 0 || a % 2==1) return 0;
+    return 1 + getLevel(a/2);
 }
- 
-int lca(int x, int y) {
-    if (msb(x) != msb(y)) {
-      return 1 << max(msb(x), msb(y));
+
+int getParent(long a) {
+    long acc = 1;
+    if(a==0) return 0;
+    int a1 = a;
+    // Gasim primul bit de 1 si in acelasi timp cea-mai-semnificativa putere a lui 2
+    while(a1%2 != 1) {
+        a1/=2;
+        acc *= 2;
     }
- 
-    int sol = 0;
- 
-    for (int b = msb(x); b >= 0; --b) {
-        if (((1 << b) & x) == ((1 << b) & y)) {
-           sol |= (1<<b) & x;
-        }
-        else if (b != 0) {
-           return sol | (1 << b);
-        }
-    }
- 
-    return sol;
+    // Pentru a ajunge din nod la parintele sau, daca numarul se termina in 01, trebuie sa adunam cea-mai-semnificativa-putere-a-lui-2;
+    // Daca se termina in 11, trebuie sa
+    if((a1 >> 1) %2 == 0)
+        return a + acc;
+    else return a - acc;
 }
- 
-int main() {
-    /*
-    for (int i = 15; i >= 1; --i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << i << ' ' << j << ' ' << lca(i, j) << ' ' << lca2(i, j) << '\n';
+
+void solveProblem() {
+   int n, a, b, aux, it;
+   scanf("%d", &n);
+
+   while(n > 0) {
+        scanf("%ld %ld", &a, &b);
+        int lvlA = getLevel(a);
+        int lvlB = getLevel(b);
+        if(lvlB < lvlA) {
+            aux = a;
+            a=b;
+            b=aux;
+            aux = lvlA;
+            lvlA=lvlB;
+            lvlB=aux;
         }
+        for (int i=0;i<(lvlB-lvlA);i++) {a=getParent(a);}
+        while(a!=b) {
+            a=getParent(a);
+            b=getParent(b);
+        }
+        printf("%ld\n",a);
+        n--;
     }
-    */
-    for (int x, y; cin >> x >> y;) {
-        cout << lca2(x, y) << "\n";
+}
+/*
+int main()
+{
+    char* in[] =  {"1.in", "2.in", "3.in", "4.in", "5.in", "6.in", "7.in", "8.in", "9.in", "10.in"};
+    char* out[] = {"1.ot", "2.ot", "3.ot", "4.ot", "5.ot", "6.ot", "7.ot", "8.ot", "9.ot", "10.ot"};
+
+    for (int i=0;i<10;i++) {
+        freopen(in[i], "r",stdin);
+        freopen(out[i],"w",stdout);
+        solveProblem();
     }
- 
+
     return 0;
 }
+
+
+*/
